cb55da3ec0f3f03fdb77dffc4aabad001840623e
Zou Lian <zoulian_medphy@sina.com>
2015-04-12 19:05:23 +0800

    cloud qa
    Signed-off-by: Zou Lian <zoulian_medphy@sina.com>

---
+++ cloud_qa/cloud_qa_main.py
@@ -0,0 +1,49 @@
+ #------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+""" A simple example plugin application.
+
+This example serves to demostrates the concepts described the accompanying
+developer crash source document.
+
+"""
+from workflow.api import WorkflowWorkbench
+
+
+if __name__ == '__main__':
+    
+
+    
+    from workflow.api import update_iconpath
+    
+    #Set the workflow icons path
+    update_iconpath(__file__)
+    
+    
+    import enaml
+    with enaml.imports():
+        from cloudqa_plugin import CloudQAManifest
+#        from enaml.workbench.core.core_manifest import CoreManifest
+#        from workflow.workflow_manifest import WorkflowManifest
+
+    workbench = WorkflowWorkbench()
+    workbench.register(CloudQAManifest())
+    
+#    workbench.register(CoreManifest())
+#    
+#    workbench.register(WorkflowManifest())
+#    
+#    
+    
+    
+    
+#    af = get_worknodes_naviagate_parameters(workbench)
+    
+    
+    
+    
+    workbench.run()
